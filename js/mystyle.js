c4611a69c8fb1d1da0583d7c9136923ae482e060
aryapj89 <aryapj244@gmail.com>
2025-02-13 10:06:57 -0600

    Add files via upload

---
+++ js/mystyle.js
@@ -0,0 +1,16 @@
+document.querySelector(".hamburger").addEventListener("click",function()
+{
+    document.querySelector(".menu").classList.toggle("show-menu")
+})
+
+document.querySelector(".hamburger").addEventListener("click",function()
+{
+    if(document.querySelector(".menu").classList.contains("show-menu"))
+    {
+        hamburgerBtn.innerHTML = '<i class="bi bi-x"></i>';
+    
+} else {
+    hamburgerBtn.innerHTML = '<i class="bi bi-list"></i> ';
+}
+})
+
